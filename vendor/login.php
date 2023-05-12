8d0c97f69a8ee2458ba8f453beda74e26b2f6793
Alex <volkogon212@gmail.com>
2023-05-12 15:15:43 +0300

    Cookies 🍪

--- vendor/login.php
+++ vendor/login.php
@@ -1,47 +1,56 @@
 <?php
+session_start()
 $username = $pswrd = "";
+if (!empty($_COOKIE["username"])) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST['username'])) {
+            $error = "Username field can't be empty!";
+            Header('Location: ../log-in.php?error='.$error);
+            exit();
+        } else {
+            $username = data_handle($_POST['username']);
+        }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST['username'])) {
-        $error = "Username field can't be empty!";
-        Header('Location: ../log-in.php?error='.$error);
-        exit();
-    } else {
-        $username = data_handle($_POST['username']);
+        if (empty($_POST['pass1'])) {
+            $error = "Password field can't be empty!";
+            Header('Location: ../log-in.php?error='.$error);
+            exit();
+        } else {
+            $pswrd = md5(data_handle($_POST['pass1']));
+        }
+
+        require_once("cfg.php");
+
+        $sql = $conn->prepare("SELECT password, full_name, email FROM users WHERE username = ?");
+        $sql->bind_param("s", $username);
+        $sql->execute();
+
+        $result = $sql->get_result();
+        $row = $result->fetch_assoc();
+        if ($row !== null) {
+            $pswrd_c = $row['password'];
+        } else {
+            $error = "User not found!";
+            Header('Location: ../log-in.php?error='.$error);
+            exit();
+        }
+
+        if ($pswrd_c === $pswrd) {
+            setcookie("username", $username, time() + (86400 * 30), "/");
+            setcookie("full_name", $row['full_name'], time() + (86400 * 30), "/");
+            setcookie("email", $row['email'], time() + (86400 * 30), "/");
+            setcookie("password", $pswrd_c, time() + (86400 * 30), "/");
+            header("Location: ../profile.php");
+            exit();
+        } else {
+            $error = "Wrong Password!";
+            Header('Location: ../log-in.php?error='.$error);
+            exit();
+        }
     }
-
-    if (empty($_POST['pass1'])) {
-        $error = "Password field can't be empty!";
-        Header('Location: ../log-in.php?error='.$error);
-        exit();
-    } else {
-        $pswrd = md5(data_handle($_POST['pass1']));
-    }
-
-    require_once("cfg.php");
-
-    $sql = $conn->prepare("SELECT password FROM users WHERE username = ?");
-    $sql->bind_param("s", $username);
-    $sql->execute();
-
-    $result = $sql->get_result();
-    $row = $result->fetch_assoc();
-    if ($row !== null) {
-        $pswrd_c = $row['password'];
-    } else {
-        $error = "User not found!";
-        Header('Location: ../log-in.php?error='.$error);
-        exit();
-    }
-
-    if ($pswrd_c === $pswrd) {
-        // Successful log in
-        echo("Success!");
-    } else {
-        $error = "Wrong Password!";
-        Header('Location: ../log-in.php?error='.$error);
-        exit();
-    }
+} else {
+    header("Location: ../profile.php");
+    exit();
 }
 
 function data_handle($data) {
